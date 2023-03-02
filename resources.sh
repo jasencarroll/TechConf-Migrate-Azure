7f0e8332885209ac0f5ae67fe5279767a85eb1fb
Jasen Carroll <11709645+jasenc@users.noreply.github.com>
2023-03-02 14:47:44 -0800

    deployed resources

---
+++ resources.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+# before we start load variables
+chmod +x variables.sh
+source variables.sh
+
+# az login
+
+# start with creating a resource group
+az group create --name $resourceGroup --location $region
+
+# create the PostgreSQL
+az postgres server create \
+  -n $elephantServer \
+  -g $resourceGroup \
+  --location $region \
+  --admin-user sql_admin \
+  --admin-password P@ssword
+
+az postgres server show \
+  --resource-group $resourceGroup \
+  --name $elephantServer
+
+# then create a storage account
+az storage account create \
+  --name $storageAccount \
+  --location $region \
+  --resource-group $resourceGroup \
+  --sku Standard_LRS \
+  --allow-blob-public-access
+
+az servicebus namespace create \
+  --name $serviceBus \
+  --resource-group $resourceGroup \
+  --location $region \
+  --sku basic
+
+az servicebus queue create \
+  --resource-group $resourceGroup \
+  --namespace-name $serviceBus  \
+  --name $queue \
+  --enable-partitioning true
+
+# afterwards create a function app
+az functionapp create \
+  --name $functionApp  \
+  --storage-account $storageAccount \
+  --consumption-plan-location $region \
+  --resource-group $resourceGroup \
+  --functions-version 3 \
+  --os-type Linux \
+  --runtime python
+
+# Finally add the storage account, like it says in the order of the README.md \
+# because the server will be available by now. 
+
+az postgres db create \
+  --name $sqlDB \
+  --resource-group $resourceGroup \
+  --server $elephantServer
---
+++ variables.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# before we start load variables
+########################################
+# General purpose variables
+# randomID=$RANDOM
+########################################
+randomID=jc521489
+resourceGroup=$"rg$randomID"
+###########################################
+# Must be unique worldwide
+region='westus3'
+########################################
+# Variables for PostgreSQL resources
+# Needs to be lower ca6e
+elephantServer="post$randomID" 
+sqlDB="techconfdb"
+storageAccount="blob$randomID"
+serviceBus="sb$randomID"
+queue="queue$randomID"
+functionApp="funcApp$randomID"
